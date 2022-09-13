276233c3647f264111d434680dc9283a8473b415
Danger Nya <igor57689907@mail.ru>
2022-09-13 17:41:05 +0700

    Добавьте файлы проекта.

---
+++ Model/ShortUrl.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+
+namespace UrlShortener.Model
+{
+    public class ShortUrl
+    {
+        public int Id { get; protected set; }
+        public string Url { get; protected set; }
+        public string UrlChunk => WebEncoders.Base64UrlEncode(BitConverter.GetBytes(Id));
+
+        public ShortUrl(Uri url)
+        {
+            Url = url.ToString();
+        }
+    }
+}
---
+++ Model/UrlDto.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Model
+{
+    public class UrlDto
+    {
+        public string Url { get; set; }
+    }
+}
